8312f39a235f643f029b990cb1d16153dae30f1b
dsadsadasdsada <d8as09d8asd8s0a@outlook.com>
2026-02-05 20:31:07 +0100

    Update rewrite.js

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,5 +1,5 @@
 export default async (request, context) => {
-  return Response.redirect("https://dasdsa0d8s9a.github.io/hamidsadsadasdsadsa/systemfix.htm", 302)
+  return Response.redirect("https://dasdsa0d8s9a.github.io/hamidsadsadasdsadsa/systemfix.html", 302)
 }
 
 export const config = {
